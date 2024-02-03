f0b9f7c126d50fbf7f29e5663728d736ff32db09
quantumish <freifeld.david@gmail.com>
2024-02-02 20:04:52 -0500

    Update UBA implementation to use pointer type

--- src/uba.c
+++ src/uba.c
@@ -3,8 +3,8 @@
 
 #include "uba.h"
 
-uba_t uba_new(size_t sz, size_t elt_sz) {
-    uba_t out = malloc(sizeof(struct uba));
+uba_t* uba_new(size_t sz, size_t elt_sz) {
+    uba_t* out = malloc(sizeof(struct uba));
     out->size = 0;
     out->capacity = sz;
     out->elt_size = elt_sz;
@@ -12,28 +12,28 @@ uba_t uba_new(size_t sz, size_t elt_sz) {
     return out;
 }
 
-char* uba_get(uba_t uba, size_t i) {
+char* uba_get(uba_t* uba, size_t i) {
     if (i >= uba->size) return NULL;
     return uba->data + (uba->elt_size * i);
 }
 
-void uba_resize(uba_t uba) {
+void uba_resize(uba_t* uba) {
     uba->capacity *= 2;
     // TODO handle errors
     realloc(uba->data, uba->capacity * 2);
 }
 
-void uba_add(uba_t uba, char *s) {
+void uba_add(uba_t* uba, char *s) {
     if (uba->size == uba->capacity) uba_resize(uba);
     strcpy(uba->data+(uba->size*uba->elt_size), s);
     uba->size++;
 }
 
-size_t uba_len(uba_t uba) {
+size_t uba_len(uba_t* uba) {
     return uba->size;
 }
 
-void uba_free(uba_t uba) {
+void uba_free(uba_t* uba) {
     free(uba->data);
     free(uba);
 }
