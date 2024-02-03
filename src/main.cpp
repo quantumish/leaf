04dd635ea87aa92a90434405f452dd28be1746d4
quantumish <freifeld.david@gmail.com>
2024-02-03 03:42:42 -0500

    Broken thread handling

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <fstream>
+#include <cstdio>
 #include <stdint.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -12,8 +13,23 @@
 
 #include <iostream>
 #include <fstream>
+#include <dirent.h>
 
 #define SLEEP_TIME 10
+
+std::vector<pid_t> get_tids(pid_t target) {
+    char path[32] = {0};
+    std::snprintf(path, 32, "/proc/%d/tasks", target);
+    printf("%s\n", path);
+    struct dirent *ep;
+    std::vector<pid_t> tids;
+    DIR* dp = opendir(path);
+    while ((ep = readdir(dp))) {
+        tids.push_back(strtoll(ep->d_name, NULL, 10));
+    }
+    closedir(dp);
+    return tids;    
+}
 
 int main(int argc, char** argv) {
     if (argc < 2) {
@@ -21,28 +37,31 @@ int main(int argc, char** argv) {
         exit(0);
     }
     pid_t target = strtoll(argv[1], NULL, 10);
-    flametree_t* ft = flametree_new();
+    std::vector<pid_t> tids = get_tids(target);
+    std::vector<flametree_t*> fts;
+    for (pid_t t : tids) fts.push_back(flametree_new());  
     // TODO do not make iteration based
     for (int i = 0; i < 10000; i++) {
-        uint32_t energy = freeze(target);
-        #ifdef NVIDIA
-        std::cout << gpu_uJ_since_ever() << "\n";
-        #endif 
-        std::vector<std::string> stack = unwind(target);
-        // for (auto s : stack) {
-        // }
-        /* for (int i = 0; i < uba_len(stack); i++) { */
-        /*     printf("%s\n", uba_get(stack, i)); */
-        /* } */
-        /* printf("\n"); */
-        unfreeze(target);
-        flametree_update(ft, stack, energy);
+        for (int i = 0; i < tids.size(); i++) {        
+            uint32_t energy = freeze(tids[i]);
+#ifdef NVIDIA
+            std::cout << gpu_uJ_since_ever() << "\n";
+#endif 
+            std::vector<std::string> stack = unwind(tids[i]);
+            
+            flametree_update(fts[i], stack, energy);
+            unfreeze(tids[i]);
+        }
         usleep(SLEEP_TIME);
     }
-    flametree_dump(ft->root_leaffn, 0);
-    std::ofstream ofs;
-    ofs.open("./out.json", std::ofstream::out | std::ofstream::app);
-    flametree_dump_json(ft, ofs);
+    // flametree_dump(ft->root_leaffn, 0);
+    for (int i = 0; i < tids.size(); i++) {    
+        std::ofstream ofs;
+        char path[32] = {0};
+        std::snprintf(path, 32, "./thread_%d.json", tids[i]);
+        ofs.open(path, std::ofstream::out);
+        flametree_dump_json(fts[i], ofs);
+    }
     // FILE* out = fopen("./test.json", "w");
     // flametree_dump(ft->root_leaffn, 0);
     /* fclose(out); */
--- src/profiler.cpp
+++ src/profiler.cpp
@@ -18,6 +18,7 @@
 #include "constants.hpp"
 #include "rapl.hpp"
 
+#include <dlfcn.h>
 #include <udis86.h>
 
 uint32_t freeze(pid_t pid) {
@@ -34,29 +35,37 @@ void unfreeze(pid_t pid) {
     ptrace(PTRACE_DETACH, pid, NULL, NULL);
 }
 
-// int disas(int pid, unsigned long addr)
-// {
-// 	ud_t ud_obj;
-// 	unsigned char buff[4];
+int disas(int pid, unsigned long addr)
+{
+	ud_t ud_obj;
+	unsigned char *buff;
+    
+    long ins = ptrace(PTRACE_PEEKTEXT, pid, addr, NULL);
+    buff = (unsigned char*)&ins;
+    // printf("%lx (%x %x %x %x)\n", ins, buff[0], buff[1], buff[2], buff[3]);
+    
+	// if (ptrace(pid, addr, buff) == -1) {
+	// 	printf("(Can't read)\n");
+	// 	return -1;
+	// }    
 
-// 	if (read_data(pid, addr, buff) == -1) {
-// 		printf("(Can't read)\n");
-// 		return -1;
-// 	}
+    int remaining = 8;
+    while (remaining > 0) {
+        ud_init(&ud_obj);
+        ud_set_input_buffer(&ud_obj, buff+(8-remaining), 64-(8*(8-remaining)));
+        ud_set_mode(&ud_obj, 64);
+        ud_set_syntax(&ud_obj, UD_SYN_ATT);
 
-// 	ud_init(&ud_obj);
-// 	ud_set_input_buffer(&ud_obj, buff, 32);
-// 	ud_set_mode(&ud_obj, 64);
-// 	ud_set_syntax(&ud_obj, UD_SYN_INTEL);
-
-// 	// ud_disassemble fills the ud_obj struct with data
-// 	if (ud_disassemble(&ud_obj) != 0) {
-// 		printf("%016lx %-20s %s\n", addr,
-// 		       ud_insn_hex(&ud_obj), ud_insn_asm(&ud_obj));
-// 	}
-
-// 	return (int)ud_insn_len(&ud_obj);
-// }
+        // ud_disassemble fills the ud_obj struct with data
+        if (ud_disassemble(&ud_obj) != 0) {
+            printf("%016lx %-20s %s\n", addr+(8-remaining),
+                   ud_insn_hex(&ud_obj), ud_insn_asm(&ud_obj));
+        } else {
+            printf("idk bro\n");
+        }
+        remaining -= (int)ud_insn_len(&ud_obj);
+    }
+}
 
 
 std::vector<std::string> unwind(pid_t pid) {    
@@ -69,9 +78,13 @@ std::vector<std::string> unwind(pid_t pid) {
         unw_word_t offset;
         char fname[MAX_SYMLEN] = {0};
         int resp = unw_get_proc_name(&c, fname, MAX_SYMLEN, &offset);
-        struct user_regs_struct regs;
-        ptrace(PTRACE_GETREGS, pid, NULL, &regs);
-        long ins = ptrace(PTRACE_PEEKTEXT, pid, regs.rip, NULL);
+        unw_word_t ip;
+        unw_get_reg(&c, UNW_REG_IP, &ip);
+        Dl_info info;
+        dladdr((void*)ip, &info);
+        printf("%p\n", (void*)ip);        
+        printf("%s: %p\n",fname, (char*)info.dli_fbase);        
+        // disas(pid, regs.rip);
         // printf("RIP: %llx instruction %lx\n", regs.rip, ins);
         stack.emplace_back(fname);
     } while(unw_step(&c) > 0);
