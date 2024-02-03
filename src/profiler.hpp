55f59598e9740e977b2f92b12a209e7dbb55f2c2
Ben Song <benedict04song@gmail.com>
2024-02-03 14:00:14 -0500

    Maybe rapl should be fixed? Also account for overflow

--- src/profiler.hpp
+++ src/profiler.hpp
@@ -3,6 +3,6 @@
 #include <unistd.h>
 #include <stdint.h>
 
-uint32_t freeze(pid_t pid, uint32_t& last_rapl);
+uint32_t freeze(pid_t pid, uint32_t* last_rapl);
 void unfreeze(pid_t pid);
 std::vector<std::string> unwind(pid_t target);
